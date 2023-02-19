dff6dcba9312eaf7efdc6af103a0d53fb1598103
jochen ade <jochen.ade@hotmail.com>
2023-02-19 16:50:53 +0100

    write to database basics working

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import numpy as np
 from os.path import splitext, exists
 from src.summary import break_up_file_to_chunks, convert_to_detokenized_text
 from nltk.tokenize import word_tokenize
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 # Get OpenAI key
 load_dotenv(".env")
@@ -19,6 +21,54 @@ filename = "imports/summary-all.csv"
 
 # Create instance of flask
 app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	views = db.Column(db.Integer, nullable=False)
+	likes = db.Column(db.Integer, nullable=False)
+
+	def __repr__(self):
+		return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
+
+resource_fields = {
+	'id': fields.Integer,
+	'name': fields.String,
+	'views': fields.Integer,
+	'likes': fields.Integer
+}
+
+class Video(Resource):
+
+	@marshal_with(resource_fields)
+	def put(self):
+		args = video_put_args.parse_args()
+
+		# Create a new video model instance with the provided data
+		video = VideoModel(name=args['name'], views=args['views'], likes=args['likes'])
+		
+		# Add the new video to the database
+		db.session.add(video)
+		db.session.commit()
+
+		# Return the new video model instance and a status code of 201
+		return video, 201
+
+api.add_resource(Video, "/video")
+
 
 # Home route
 @app.route("/")
