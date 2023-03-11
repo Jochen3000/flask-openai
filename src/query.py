2e2aeaa825bc768887e6465de6514b805ca0a69a
jochen ade <jochen.ade@hotmail.com>
2023-03-11 14:01:50 +0100

    added context to query response

--- src/query.py
+++ src/query.py
@@ -21,9 +21,34 @@ def get_answer():
 
     question = data['chatPrompt']
     result = answer_question(df, question=question)
-    return jsonify({"botResponse": result})
+    return jsonify(result)
 
 def create_context(question, df, max_len=1800, size="ada"):
+
+    # Get the embeddings for the question
+    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+    # Get the distances from the embeddings
+    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+        
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+        
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+        
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+
+    # Return the context
+    return returns
+
 
     # Get the embeddings for the question
     q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
@@ -85,7 +110,7 @@ def answer_question(
             stop=stop_sequence,
             model=model,
         )
-        return response["choices"][0]["text"].strip()
+        return {"context": context, "botResponse": response["choices"][0]["text"].strip()}
     except Exception as e:
         print(e)
         return ""
